a11c853478acb85e02c8889ab7a13465da2cd18b
William Stewart <hmaswill@gmail.com>
2020-05-17 11:57:29 +0800

    Changed name to lowercase

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Personal-Website app is running!');
+    expect(page.getTitleText()).toEqual('personal-website app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Personal-Website'`, () => {
+  it(`should have as title 'personal-website'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Personal-Website');
+    expect(app.title).toEqual('personal-website');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Personal-Website app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('personal-website app is running!');
   });
 });
