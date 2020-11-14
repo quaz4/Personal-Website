2baedce5a1e9dd0086c2e8147f2feb9cbbc1af74
William Stewart <hmaswill@gmail.com>
2020-11-14 16:08:08 +0800

    Angular 11 Update

--- src/app/timeline/timeline-entry/timeline-entry.component.spec.ts
+++ src/app/timeline/timeline-entry/timeline-entry.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { TimelineEntryComponent } from './timeline-entry.component';
 
@@ -6,7 +6,7 @@ describe('TimelineEntryComponent', () => {
   let component: TimelineEntryComponent;
   let fixture: ComponentFixture<TimelineEntryComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ TimelineEntryComponent ]
     })
