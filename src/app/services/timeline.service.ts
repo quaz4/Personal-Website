d9b2594797c537f869f55f3947eaa5fd61588cb3
William Stewart <hmaswill@gmail.com>
2021-06-06 12:44:08 +0800

    Bump packages

--- src/app/services/timeline.service.ts
+++ src/app/services/timeline.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { experience, projects, certs } from './data.js';
+import { experience, projects, certs } from './data';
 
 @Injectable({
   providedIn: 'root'
